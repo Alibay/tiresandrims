5ae7e743df37ac9d9921727b93763f9d4c2b5e6d
Ibraim Alibay <ibraim.alibay@gmail.com>
2017-07-15 16:39:38 +0300

    new components

---
+++ resources/assets/js/components/car/wheel-model/SelectWheelModel.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="row">
+        <div class="col-md-6">
+            <div class="form-group">
+                <label>Производитель</label>
+                <select2
+                        :options="[]"
+                        :change="onManufacturerChange"
+                        :minimumResultsForSearch="5"
+                        v-model="selectedManufacturer"
+                        defaultText="Выберите производитель"
+                        :disabled="selectedBrand == 0">
+                </select2>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="form-group">
+                <label>Модель</label>
+                <select2
+                        :options="[]"
+                        :change="onManufacturerModelChange"
+                        :minimumResultsForSearch="5"
+                        v-model="selectedManufacturerModel"
+                        defaultText="Выберите модель"
+                        :disabled="selectedBrand == 0">
+                </select2>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['data']
+    }
+</script>
